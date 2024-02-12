bfc3012c85c0d1477484ea28f9673be0e92eb081
Admin <ndn153@naver.com>
2024-02-12 22:40:38 +0900

    Ver1.Setting

---
+++ src/main/java/hello/toCU/ToCuApplication.java
@@ -0,0 +1,13 @@
+package hello.toCU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToCuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToCuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/toCU/ToCuApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.toCU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToCuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
